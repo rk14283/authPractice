c3f3c11f3d0f43879abdb576e121b85da75b6452
rk14283 <rk14283@my.bristol.ac.uk>
2023-02-08 22:05:13 +0100

    auth email password with zod

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 var path = require("path");
+const z = require("zod");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 var app = express();
@@ -26,21 +27,43 @@ app.listen(PORT, () => {
   console.log("Server listening on http://localhost:" + PORT);
 });
 
+let emailSchema = z
+  .string()
+  .min(2, { message: "Must be 2 or more characters long" });
+
+let passwordSchema = z
+  .string()
+  .min(2, { message: "Must be 2 or more characters long" });
+
 app.post("/signup", async (req, res) => {
-  let email = String(req.body.email);
-  //console.log(email);
-  let password = String(req.body.password);
-  //console.log(password);
-  let hash = bcrypt.hashSync(password, SALT_ROUNDS);
-  let userInfo = {};
-  userInfo = {
-    Email: email,
-    Password: hash,
-  };
-  //console.log(hash);
-  let addedInfo = await prisma.userInfo.create({ data: userInfo });
-  console.log(addedInfo);
-  res
-    .status(201)
-    .json({ message: "New user added to database", userInfo: userInfo });
+  try {
+    //let email = String(req.body.email);
+    let email = String(emailSchema.parse(req.body.email));
+    console.log(email);
+    //let password = String(req.body.password);
+    let password = String(passwordSchema.parse(req.body.password));
+    //console.log(password);
+    let hash = bcrypt.hashSync(password, SALT_ROUNDS);
+    let userInfo = {};
+    userInfo = {
+      Email: email,
+      Password: hash,
+    };
+    // //console.log(hash);
+    let addedInfo = await prisma.userInfo.create({ data: userInfo });
+    console.log(addedInfo);
+    res
+      .status(201)
+      .json({ message: "New user added to database", userInfo: userInfo });
+  } catch (error) {
+    console.log(error.issues, error.name);
+
+    if (error.name === "ZodError") {
+      return res
+        .status(400)
+        .json({ message: "validation error", errors: error.issues });
+    } else {
+      return res.status(500).json({ message: "Something went wrong sorry!" });
+    }
+  }
 });

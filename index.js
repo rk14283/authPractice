6d96cd0eae260a287c5d7b9f05e60bcbb7aea21e
rk14283 <rk14283@my.bristol.ac.uk>
2023-02-14 13:41:17 +0100

    login session using database

--- index.js
+++ index.js
@@ -8,29 +8,16 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 var app = express();
 const bcrypt = require("bcrypt");
-const { Console } = require("console");
 SALT_ROUNDS = 10;
 app.use(bodyParser.json());
-//what is it doing here
+//what is it doing here, html form url encoded
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.set("view engine", "ejs");
 const { v4: uuidv4 } = require("uuid");
-const { json } = require("body-parser");
-const sessions = {};
 app.set("views", path.join(__dirname, "./views"));
-
-class Session {
-  constructor(email, expiresAt) {
-    this.email = email;
-    this.expiresAt = expiresAt;
-  }
-  isExpired() {
-    this.expiresAt < new Date();
-  }
-}
 
 app.get("/", async (req, res) => {
   res.render("templates");
@@ -89,21 +76,13 @@ app.post("/login", (req, res) => {
         email: email,
       },
     });
-    //    console.log(matchingEmail.password);
 
     if (user != null) {
       let passwordMatch = bcrypt.compareSync(password, user.password);
-      //console.log(passwordMatch);
       if (passwordMatch === true) {
         const sessionToken = uuidv4();
-        //  console.log(sessionToken);
         const now = new Date();
-        const expiresAt = new Date(+now + 240 * 1000);
-        //console.log(expiresAt);
-        const session = new Session(user.email, expiresAt);
-        //    console.log(session);
-        sessions[sessionToken] = session;
-        //console.log(sessions);
+        const expiresAt = new Date(+now + 30 * 1000);
         sessionData = {
           token: sessionToken,
           email: user.email,
@@ -112,11 +91,9 @@ app.post("/login", (req, res) => {
         const newSession = await prisma.session.create({
           data: sessionData,
         });
-        //console.log(newSession);
-        res.cookie("session_token", sessionToken, { expires: expiresAt });
-        //res.send(sessionToken);
-        //console.log(sessionToken);
-        // return res.status(200).json({ message: "logged in" });
+        res.cookie("session_token", sessionToken, {
+          expires: new Date(+expiresAt + 60000),
+        });
         return res.redirect("/welcome");
       } else if (passwordMatch === false) {
         return res.status(403).json({ message: "password incorrect" }).end();
@@ -133,50 +110,31 @@ app.get("/login", async (req, res) => {
 });
 
 app.get("/welcome", async (req, res) => {
-  console.log("hello", req.cookies);
-
-  if (!req.cookies) {
+  if (!req.cookies.session_token) {
     return res.redirect("/login");
   }
-  //const sessionToken = req.cookies["session_token"];
 
-  const session = prisma.session.findFirst({
+  const session = await prisma.session.findFirst({
     where: {
       token: req.cookies["session_token"],
     },
   });
 
-  //const sessionToken = null;
-  let tokenFromDB = await session;
-  //console.log("this is token outside function", await sessionToken);
-  //console.log("this is your token", token.token);
-  let sessionToken = tokenFromDB.token;
-  //console.log(sessionToken);
-
-  if (!sessionToken) {
-    // If the cookie is not set, return an unauthorized status
+  if (!session) {
     return res.redirect("/login");
   }
-  //userSession = sessions[sessionToken];
-  // let userSession = sessionToken;
-  // if (!userSession) {
-  //   // If the session token is not present in session map, return an unauthorized error
-  //   return res.redirect("/login");
-  // }
-
-  //console.log(tokenFromDB.expiry);
-  //let expiryTime = tokenFromDB.expiry.getTime();
-  let expiryTime = tokenFromDB.expiry;
-  //console.log(Date.now() / 1000);
+  let expiryTime = session.expiry;
 
   if (expiryTime <= +new Date()) {
-    //delete sessions[sessionToken];
-    const deletedUserToken = await prisma.session.deleteMany({
-      where: {},
+    res.redirect("/login");
+
+    const deletedUserToken = await prisma.session.delete({
+      where: {
+        id: session.id,
+      },
     });
-    //console.log(deletedUserToken);
-    return res.redirect("/login");
+    return console.log(deletedUserToken);
   }
 
-  res.send(`Welcome ${tokenFromDB.email}!`).end();
+  res.send(`Welcome ${session.email}!`).end();
 });
